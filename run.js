bf21995bff2d13bcf409f3f261fb42f2b5a8ba68
douglas <douglasms90@gmail.com>
2020-07-05 17:00:14 -0300

    modules: sequelize and handlebars

--- run.js
+++ run.js
@@ -1,15 +1,36 @@
 const express = require("express")
 const app = express()
+const Sequelize  = require("sequelize")
+const handlebars = require("express-handlebars")
 
 
-app.get("/", function(Req, res){
-  res.sendfile(__dirname + "/templates/index.html")
-})
+// Config
+  // Template Engine
+    app.engine("handlebars", handlebars({defaultLayout: "main"}))
+		app.set("view engine", "handlebars")
 
-app.get("/suporte", function(req, res){
-  res.sendfile(__dirname + "/templates/suporte.html")
-})
+	// Conexão com o banco de dados
+		const sequelize = new Sequelize("test", "root", "123456", {
+			host: "localhost",
+			dialect: "mysql"
+		})
 
+// Rotas
+		app.get("/cad", function(req, res){
+			res.render("formulario")
+		})
+
+		app.get("/add", function(req, res){
+			res.send('FORMULARIO')
+		})
+
+///app.get("/", function(req, res){
+//  res.sendfile(__dirname + "/views/templates/index.html")
+//})
+
+//app.get("/suporte", function(req, res){
+//  res.sendfile(__dirname + "/views/templates/suporte.html")
+//})
 
 app.listen(5100, function(){
   console.log("Server rodando...http://127.0.0.1:5100")
