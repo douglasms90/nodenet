cdf6dd083868c6549c4dfe7b985a368118db20ef
douglasms90 <douglasms90@gmail.com>
2020-07-05 21:47:09 -0300

    public admin

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/", function(req, res) {
+	res.render("admin/index")
+})
+
+router.get("/", function(req, res) {
+	res.render("admin/index")
+})
+
+router.get("/", function(req, res) {
+	res.render("admin/index")
+})
+
+router.get("/", function(req, res) {
+	res.render("admin/index")
+})
+
+module.exports = router
--- run.js
+++ run.js
@@ -1,37 +1,33 @@
 const express = require("express")
-const app = express()
-const Sequelize  = require("sequelize")
 const handlebars = require("express-handlebars")
+const bodyParser = require("body-parser")
+const Sequelize  = require("sequelize")
+const path = require("path")
+const app = express()
+const admin = require("./routes/admin")
 
 
-// Config
-  // Template Engine
+// Configurações
+  // Body Parser
+		app.use(bodyParser.urlencoded({extended: false}))
+		app.use(bodyParser.json())
+	// Handlebars
     app.engine("handlebars", handlebars({defaultLayout: "main"}))
 		app.set("view engine", "handlebars")
-
 	// Conexão com o banco de dados
-		const sequelize = new Sequelize("test", "root", "123456", {
-			host: "localhost",
-			dialect: "mysql"
-		})
-
+		//...
+	// Public
+		app.use(express.static(path.join(__dirname, "public")))
 // Rotas
 		app.get("/cad", function(req, res){
 			res.render("formulario")
 		})
 
-		app.get("/add", function(req, res){
-			res.send('FORMULARIO')
+		app.post("/add", function(req, res){
+			res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo)
 		})
 
-///app.get("/", function(req, res){
-//  res.sendfile(__dirname + "/views/templates/index.html")
-//})
-
-//app.get("/suporte", function(req, res){
-//  res.sendfile(__dirname + "/views/templates/suporte.html")
-//})
-
+// Outros
 app.listen(5100, function(){
   console.log("Server rodando...http://127.0.0.1:5100")
 })
